5bcc7d33bb090eeb3707b99341d055afc8461bc7
corvainx <ishmeetsingharora07@gmail.com>
2026-08-22 20:51:41 +0530

    scripts

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+export PATH=$PATH:$HOME/.cargo/bin
+dufs -p 8000 ~/storage/cloud --allow-upload
---
+++ server-setup.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+set -e
+
+echo "==> Installing Rust"
+pkg install -y rust
+
+echo "==> Installing dufs (simple web file server with upload GUI)"
+cargo install dufs
+
+echo "==> Adding cargo bin to PATH"
+if ! grep -q '.cargo/bin' ~/.bashrc 2>/dev/null; then
+  echo 'export PATH=$PATH:$HOME/.cargo/bin' >>~/.bashrc
+fi
+export PATH=$PATH:$HOME/.cargo/bin
+
+echo "==> Setup complete"
+echo ""
+echo "To start the file server:"
+echo "  dufs -p 8000 ~/storage/cloud --allow-upload"
+echo ""
+echo "Access it at:"
+echo "  http://localhost:8000        (on this phone)"
+echo "  http://<phone-ip>:8000       (from other devices, e.g. Arch laptop)"
